0b008dd1e8175aa3f4616d380ce048a6ff74973a
GithubWaddle <wafiwaffleuni@gmail.com>
2025-06-12 23:12:20 +0700

    Made interfaces for vigenere_cipher.py and vigenere_cipher_breaker.py

---
+++ Vigenere_Breaker/vigenere_cipher.py
@@ -0,0 +1,11 @@
+# for use in normally encoding or decoding vigenere ciphers
+
+
+def encrypt(plaintext: str, key: str) -> str:
+	ciphertext: str = ""
+	return ciphertext
+
+
+def decrypt(ciphertext: str, key: str) -> str:
+	plaintext: str = ""
+	return plaintext
---
+++ Vigenere_Breaker/vigenere_cipher_breaker.py
@@ -0,0 +1,12 @@
+# breaking a vigenere cipher without knowing the key
+
+
+def break(ciphertext: str) -> str:
+	plaintext: str = ""
+	return plaintext
+
+
+# for visualization of execution on website frontend
+def break_with_polling(ciphertext: str) -> str:
+	plaintext: str = ""
+	return plaintext
