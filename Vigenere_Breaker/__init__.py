c7f2b7c9745295c6a4dcfe48d157cbedbf58062d
gamerzord <gamerzord76@gmail.com>
2025-06-14 11:18:58 +0700

    gzcommit

--- Vigenere_Breaker/__init__.py
+++ Vigenere_Breaker/__init__.py
@@ -1,8 +1,63 @@
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from vigenere_cipher import vigenere_encrypt, vigenere_decrypt
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
 
+@app.route('/encrypt', methods=['GET', 'POST'])
+def encrypt():
+    if request.method == 'POST':
+        plain_text = request.form['plaintext']
+        key = request.form.get('key')
+
+        if not key:
+            result, key = vigenere_encrypt(plain_text)
+        else:
+            result, _ = vigenere_encrypt(plain_text, key)
+
+        return jsonify(result=result, key=key)
+
+    elif request.method == 'GET':
+        text = request.args.get('text')
+        key = request.args.get('key')
+        response_type = request.args.get('type', 'json')
+
+        if not text:
+            return jsonify(error="Text is required"), 400
+
+        if not key:
+            result, key = vigenere_encrypt(text)
+        else:
+            result, _ = vigenere_encrypt(text, key)
+
+        if response_type == "json":
+            return jsonify(result=result, key=key)
+        else:
+            return result
+
+@app.route('/decrypt', methods=['GET', 'POST'])
+def decrypt():
+    if request.method == 'POST':
+        encrypted_text = request.form['encryptedText']
+        key = request.form['key']
+        if not key:
+            return jsonify(error="Key is required"), 400
+        result = vigenere_decrypt(encrypted_text, key)
+        return jsonify(result=result)
+
+    elif request.method == 'GET':
+        text = request.args.get('text')
+        key = request.args.get('key')
+        response_type = request.args.get('type', 'json')
+
+        if not text or not key:
+            return jsonify(error="Both text and key are required"), 400
+
+        result = vigenere_decrypt(text, key)
+
+        if response_type == "json":
+            return jsonify(result=result)
+        else:
+            return result
--- Vigenere_Breaker/vigenere_cipher.py
+++ Vigenere_Breaker/vigenere_cipher.py
@@ -1,11 +1,51 @@
-# for use in normally encoding or decoding vigenere ciphers
+import random
+import string
+
+def generate_key(length):
+    return ''.join(random.choices(string.ascii_letters, k=length))
+
+def vigenere_encrypt(plaintext, key=None):
+    if key is None:
+        key = generate_key(len([c for c in plaintext if c.isalpha()]))
+
+    counter = 0
+    encoded_text = ""
+    for char in plaintext:
+        if char.isalpha():
+            vig_key = key[counter % len(key)]
+            vig_key_val = ord(vig_key.lower()) - ord('a')
+
+            if char.islower():
+                vig_char = chr(((ord(char) - ord("a") + vig_key_val) % 26) + ord("a"))
+            else:
+                vig_char = chr(((ord(char) - ord("A") + vig_key_val) % 26) + ord("A"))
+
+            counter += 1
+        else:
+            vig_char = char
+
+        encoded_text += vig_char
+
+    return encoded_text, key
 
 
-def encrypt(plaintext: str, key: str) -> str:
-	ciphertext: str = ""
-	return ciphertext
+def vigenere_decrypt(encodedtext, key):
+    counter = 0
+    decoded_text = ""
+    for char in encodedtext:
+        if char.isalpha():
+            vig_key = key[counter % len(key)]
+            vig_key_val = ord(vig_key.lower()) - ord('a')
 
+            if char.islower():
+                vig_char = chr(((ord(char) - ord("a") - vig_key_val) % 26) + ord("a"))
+            else:
+                vig_char = chr(((ord(char) - ord("A") - vig_key_val) % 26) + ord("A"))
 
-def decrypt(ciphertext: str, key: str) -> str:
-	plaintext: str = ""
-	return plaintext
+            counter += 1
+        else:
+            vig_char = char
+
+        decoded_text += vig_char
+
+    return decoded_text
