7cb367c54d385e37ee60490020e126e9eaa3bde7
Waddle <144913910+GithubWaddle@users.noreply.github.com>
2025-06-21 18:48:01 +0700

    Update vigenere_cipher.py

--- Vigenere_Breaker/vigenere_cipher.py
+++ Vigenere_Breaker/vigenere_cipher.py
@@ -1,51 +1,33 @@
-import random
 import string
 
-def generate_key(length):
-    return ''.join(random.choices(string.ascii_letters, k=length))
 
-def vigenere_encrypt(plaintext, key=None):
-    if key is None:
-        key = generate_key(len([c for c in plaintext if c.isalpha()]))
+def clean_text(text):
+    return ''.join(filter(str.isalpha, text.upper()))
 
-    counter = 0
+
+def vigenere_encrypt(plaintext, key):
+    plaintext = clean_text(plaintext)
+    key = clean_text(key)
+
     encoded_text = ""
-    for char in plaintext:
-        if char.isalpha():
-            vig_key = key[counter % len(key)]
-            vig_key_val = ord(vig_key.lower()) - ord('a')
+    for i, char in enumerate(plaintext):
+        key_char = key[i % len(key)]
+        shift = ord(key_char) - ord('A')
+        encrypted_char = chr(((ord(char) - ord('A') + shift) % 26) + ord('A'))
+        encoded_text += encrypted_char
 
-            if char.islower():
-                vig_char = chr(((ord(char) - ord("a") + vig_key_val) % 26) + ord("a"))
-            else:
-                vig_char = chr(((ord(char) - ord("A") + vig_key_val) % 26) + ord("A"))
-
-            counter += 1
-        else:
-            vig_char = char
-
-        encoded_text += vig_char
-
-    return encoded_text, key
+    return encoded_text
 
 
 def vigenere_decrypt(encodedtext, key):
-    counter = 0
+    encodedtext = clean_text(encodedtext)
+    key = clean_text(key)
+
     decoded_text = ""
-    for char in encodedtext:
-        if char.isalpha():
-            vig_key = key[counter % len(key)]
-            vig_key_val = ord(vig_key.lower()) - ord('a')
-
-            if char.islower():
-                vig_char = chr(((ord(char) - ord("a") - vig_key_val) % 26) + ord("a"))
-            else:
-                vig_char = chr(((ord(char) - ord("A") - vig_key_val) % 26) + ord("A"))
-
-            counter += 1
-        else:
-            vig_char = char
-
-        decoded_text += vig_char
+    for i, char in enumerate(encodedtext):
+        key_char = key[i % len(key)]
+        shift = ord(key_char) - ord('A')
+        decrypted_char = chr(((ord(char) - ord('A') - shift) % 26) + ord('A'))
+        decoded_text += decrypted_char
 
     return decoded_text
